23ff3282343ba5e62ef60da031f3067e63ce39bd
gak5275 <gak5275@psu.edu>
2023-10-23 16:50:04 -0400

    got animated shapes to appear

--- gak3d2/main.js
+++ gak3d2/main.js
@@ -1,30 +1,9 @@
-
+import * as THREE from 'three';
 import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
+/*import javascriptLogo from './javascript.svg'
+import viteLogo from '/vite.svg'*/
 import { setupCounter } from './counter.js'
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
-
-setupCounter(document.querySelector('#counter'))
-
-import * as THREE from 'three';
 const scene = new THREE.Scene();
 
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
@@ -90,4 +69,3 @@ function animate() {
 }
 
 animate();
-
